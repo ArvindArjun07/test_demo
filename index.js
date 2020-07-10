afeaab3f096eeba67ba35eecc25ae4a169862503
ArvindArjun07 <aravindcse.rymec@gmail.com>
2020-07-10 15:05:23 +0530

    second node commit

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var rect = {
+/*var rect = {
 	perimeter : (x,y) => (2*(x+y)),
 	area : (x,y) => (x*y)
 };
+*/
+//replacement to the upper part and use require module to import external file
+
+var rect = require('./rectangle');
 
 function solveRect(l, b){
 	console.log("length l = " + l + " and breadth b = " + b);
 
 	if(l<=0 || b<=0){
-		console.log("length and breadthshould not be zero l= " + l + " b = " + b);
+		console.log("length and breadth should be greater than zero");
 	}
 	else{
 		console.log("area of rectangle = " + rect.area(l, b));
@@ -18,3 +22,5 @@ function solveRect(l, b){
 solveRect(4, 6);
 solveRect(0, 4);
 solveRect(4, 0);
+solveRect(2, 8);
+solveRect(6, 8);
