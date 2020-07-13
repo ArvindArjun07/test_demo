0465a6aff0bd2b9adadc00fb74600cfcbf0c43b5
ArvindArjun07 <aravindcse.rymec@gmail.com>
2020-07-13 11:08:56 +0530

    callback commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 //replacement to the upper part and use require module to import external file
 
+/*
 var rect = require('./rectangle');
 
 function solveRect(l, b){
@@ -22,3 +23,32 @@ function solveRect(l, b){
 solveRect(4, 6);
 solveRect(0, 4);
 solveRect(6, 8);
+
+*/
+
+var rect = require('./rectangle');
+
+function solveRect(l, b){
+	//console.log("length l = " + l + " and breadth b = " + b);
+
+	rect(l, b,(err, rectangle)=> {
+		if(err){
+			console.log("ERROR:" , err.message);
+		}
+		else{
+			console.log("area of rectangle = " + rectangle.area());
+			console.log("perimeter of rectangle = " +rectangle.perimeter());
+		}
+	});
+	
+}
+
+
+solveRect(4, 6);
+solveRect(0, 4);
+solveRect(6, 8);
+solveRect(0, 6);
+solveRect(0, 4);
+solveRect(5, 0);
+
+
